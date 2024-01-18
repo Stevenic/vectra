e5450b9a8fd2ab44cc232047ebcd6a735966af74
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-18 17:07:29 +0000

    updated fs

--- src/fs.ts
+++ src/fs.ts
@@ -7,3 +7,11 @@ export async function access(path: string) {
 export async function readText(path: string): Promise<string> {
     return (await fs.readFile(path)).toString()
 }
+
+export async function unlink(path: string) {
+    await fs.unlink(path)
+}
+
+export async function writeFile(path: string, data: string) {
+    await fs.writeFile(path, data)
+}
