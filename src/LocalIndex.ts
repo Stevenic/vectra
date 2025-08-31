51ce650a756e8108ff98a1a927142a97425b03e4
Julien Bertazzo Lambert <jbertazzolambert@gmail.com>
2025-08-31 17:05:27 -0400

    force deep copy of data on begin update

--- src/LocalIndex.ts
+++ src/LocalIndex.ts
@@ -66,7 +66,7 @@ export class LocalIndex<TMetadata extends Record<string,MetadataTypes> = Record<
         }
 
         await this.loadIndexData();
-        this._update = Object.assign({}, this._data);
+        this._update = Object.assign({}, structuredClone(this._data));
     }
 
     /**
